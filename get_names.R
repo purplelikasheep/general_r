8eaaf59734971fe4030ec2ee361916d3044d3239
Rafael Viegas <purplelikasheep@gmail.com>
2018-04-24 09:03:32 -0300

    inicial commit, olnly get_names

---
+++ get_names.R
@@ -0,0 +1,26 @@
+
+get_names <- function(path, pattern){
+
+    if(missing(pattern)) {
+        full_names <- list.files(path, full.names=T)
+    } else {
+        full_names <- list.files(path, full.names=T, pattern=pattern)
+    }
+
+    basenames <- basename(full_names)
+
+    basenames_no_extension <- strsplit(basenames, split='[.]')
+    basenames_no_extension <- unlist(basenames_no_extension)
+    basenames_no_extension <- matrix(basenames_no_extension, nrow=2)
+    basenames_no_extension <- basenames_no_extension[1, ]
+
+    dt <- data.frame(
+        full_name=full_names,
+        basename=basenames,
+        basename_no_extension=basenames_no_extension,
+        stringsAsFactors=F
+    )
+
+    return(dt)
+
+}
