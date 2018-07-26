92320d1d813692a9779b27fc81a8bdd7281b549b
Rafael Viegas <purplelikasheep@gmail.com>
2018-07-26 14:16:07 -0300

    better version get_names

--- generics.R
+++ generics.R
@@ -7,12 +7,23 @@ get_names <- function(path, pattern){
         full_names <- list.files(path, full.names=T, pattern=pattern)
     }
 
-    basenames <- basename(full_names)
+    if (length(full_names)!=0){
 
-    basenames_no_extension <- strsplit(basenames, split='[.]')
-    basenames_no_extension <- unlist(basenames_no_extension)
-    basenames_no_extension <- matrix(basenames_no_extension, nrow=2)
-    basenames_no_extension <- basenames_no_extension[1, ]
+        basenames <- basename(full_names)
+
+        basenames_no_extension <- strsplit(basenames, split='[.]')
+        basenames_no_extension <- unlist(basenames_no_extension)
+        basenames_no_extension <- matrix(basenames_no_extension, nrow=2)
+        basenames_no_extension <- basenames_no_extension[1, ]
+
+    } else {
+
+        basenames <- c()
+        basenames_no_extension <- c()
+
+    }
+
+
 
     dt <- data.frame(
         full_name=full_names,
@@ -38,7 +49,7 @@ spatialFilter <- function(r, fileOut, agricKind){
 
     size <- 5
 	w <- matrix(data=1, ncol=size, nrow=size)
-	r <- focal(r, w=w, fun = max, na.rm=T, pad = T, padValue = 1)
+	r <- focal(r, w=w, fun = max, na.rm=T, pad = T, padValue = 0)
 	r <- reclassify(r, c(-Inf, 15, 0, 15, Inf, agricKind), filename = fileOut, format = 'GTiff')
 	return(r)
 
