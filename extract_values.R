ed4ba9a0fab01c30be6f583e1016f54d753a219b
Rafael Viegas <purplelikasheep@gmail.com>
2018-07-30 15:52:02 -0300

    sl

--- extract_values.R
+++ extract_values.R
@@ -7,43 +7,17 @@ library('doSNOW')
 
 
 
-source('generics.R')
+source('/home/rviegas/Dropbox/general_r/generics.R')
 
 
 
 wd <- '/media/data/sao_domingos_de_prata'
-path_shps <- 'shp/points'
-path_tifs <- '/run/media/rviegas/My Passport/rafael/sao_domingos_de_prata/vars'
-path_tabelas_randomForst <- 'tabelas_randomForest'
+path_shps <- '/media/data/suzando/ma5/shp/talhoes_points'
+path_tifs <- '/media/data/cHLS/v2/noise/out/noise_mask_all'
+path_tabelas <- '/media/data/cHLS/v2/noise/out/summary_csvs'
 
 vars <- c(
-    'blue_savg',
-    'bndvi',
-    'brightness',
-    'ctvi',
-    'eta',
-    'fifth',
-    'fourth',
-    'gli',
-    'green_savg',
-    'h',
-    'if',
-    'nbr_diss',
-    'nbr_dvar',
-    'nbr_savg',
-    'ndvi_savg',
-    'nir',
-    'nir_savg',
-    'pndvi',
-    'rdi',
-    'red',
-    'red_savg',
-    'ri',
-    'sixth',
-    'swir1',
-    'swir1_savg',
-    'vari',
-    'water'
+    'noise'
 )
 
 
@@ -61,6 +35,9 @@ i <- 6
 
 # for ( i in 1:length(tifs$full_name)){
 #
+shp <- readOGR(path_shps,'LC08_23MKR-222063_20140808_cHLS', verbose=F)
+
+
 cello <- makeCluster(4, outfile="")
 registerDoSNOW(cello)
 
@@ -75,38 +52,50 @@ l <- foreach(
 
     print(i)
 
-    dummy_shp <- shps$basename_no_extension
-    dummy_tif <- tifs$basename_no_extension[i]
-
-    ind  <- which(
-        dummy_tif == dummy_shp
-    )
-    shp <- readOGR(path_shps, shps$basename_no_extension[ind])
-
-    r <- stack(tifs$full_name[i])
-    names(r) <- vars
-
-    ext <- extract(
-        r,
-        shp
-    )
-
-    a <- cbind.data.frame(
-        shp@data,
-        ext
-    )
-
     name_out <- paste(
-        path_tabelas_randomForst,
+        path_tabelas,
         tifs$basename_no_extension[i],
         sep='/'
     )
 
-    write.csv(a, name_out)
-    a
+    name_out <- paste0(
+        name_out,
+        '.csv'
+    )
 
+    alreadydone <- get_names(path_tabelas, '\\.csv$')
+
+
+    if (!(grepl(name_out, alreadydone))){
+
+        # dummy_shp <- shps$basename_no_extension
+        # dummy_tif <- tifs$basename_no_extension[i]
+        #
+        # ind  <- which(
+        #     dummy_tif == dummy_shp
+        # )
+        # shp <- readOGR(path_shps, shps$basename_no_extension[ind])
+
+        r <- stack(tifs$full_name[i])
+        names(r) <- vars
+
+        shp <- spTransform(shp, proj4string(r))
+        ext <- extract(
+            r,
+            shp
+        )
+
+        a <- cbind.data.frame(
+            shp@data,
+            ext
+        )
+
+
+        write.csv(a, name_out)
+        a
+    }
 
 }
 stopCluster(cello)
 
-write.csv(l, 'tables/all.csv')
+# write.csv(l, 'tables/all.csv')
